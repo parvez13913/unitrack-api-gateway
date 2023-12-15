6e0fca3362617c91400070cb89365772159b7ff5
parvez13913 <parvezz13913@gmail.com>
2023-12-15 16:47:57 +0600

    [DONE] OfferedCourseSectionValidation

--- src/app/modules/OfferedCourseSection/OfferedCourseSection.route.ts
+++ src/app/modules/OfferedCourseSection/OfferedCourseSection.route.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { OfferedCourseSectionController } from './OfferedCourseSection.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { OfferedCourseSectionValidation } from './OfferedCourseSection.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -7,10 +11,24 @@ router.get('/', OfferedCourseSectionController.getAllOfferedCourseSections);
 
 router.get('/:id', OfferedCourseSectionController.getSingleOfferedCourseSection);
 
-router.post('/', OfferedCourseSectionController.createOfferedCourseSection);
+router.post(
+  '/',
+  validateRequest(OfferedCourseSectionValidation.createOfferedCourseSectionZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.createOfferedCourseSection
+);
 
-router.patch('/:id', OfferedCourseSectionController.updateOfferedCourseSection);
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseSectionValidation.updateOfferedCourseSectionZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.updateOfferedCourseSection
+);
 
-router.delete('/:id', OfferedCourseSectionController.deleteOfferedCourseSection);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.deleteOfferedCourseSection
+);
 
 export const OfferedCourseSectionRoutes = router;
