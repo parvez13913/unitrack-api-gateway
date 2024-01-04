3c7c94facaea722b9f7cc49e939ea7c7966a47d9
parvez13913 <parvezz13913@gmail.com>
2024-01-04 18:58:29 +0600

    [DONE] Set refreshToken into browser cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,11 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
 import { AuthenticationSerice } from './auth.service';
 import sendResponse from '../../../shared/response';
+import config from '../../../config';
+import httpStatus from 'http-status';
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await AuthenticationSerice.loginUser(req);
-    sendResponse(res, result);
+
+    const { refreshToken, ...others } = result.data;
+
+    // Set refreshToken into browser cookie
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true
+    };
+    res.cookie('refreshToken', result?.data?.refreshToken, cookieOptions);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User loged in !',
+      data: others
+    });
   } catch (error) {
     next(error);
   }
