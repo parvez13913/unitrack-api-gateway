9d167544509058d3089bdf6435538189bdc45a8c
parvez13913 <parvezz13913@gmail.com>
2024-01-05 15:44:50 +0600

    [DONE] login and refreshToken zod validation

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { AuthenticationController } from './auth.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AuthenticationValidation } from './auth.validation';
 
 const router = express.Router();
 
-router.post('/login', AuthenticationController.loginUser);
+router.post(
+  '/login',
+  validateRequest(AuthenticationValidation.loginZodSchema),
+  AuthenticationController.loginUser
+);
 
-router.post('/refreshToken', AuthenticationController.refreshToken);
+router.post(
+  '/refreshToken',
+  validateRequest(AuthenticationValidation.refreshTokenZodSchema),
+  AuthenticationController.refreshToken
+);
 
 export const AuthenticationRoutes = router;
