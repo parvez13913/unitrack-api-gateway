d3719ae8b28bbe67c0a0eb3bfc97009b2320e2be
parvez13913 <parvezz13913@gmail.com>
2024-01-07 18:48:27 +0600

    [DONE] Student validation

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { StudentController } from './student.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentValidation } from './student.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -7,16 +11,29 @@ router.get('/', StudentController.getAllStudents);
 
 router.get('/profile/:id', StudentController.getStudentProfile);
 
-router.get('/myCourses', StudentController.getMyCourses);
+router.get('/myCourses', auth(ENUM_USER_ROLE.STUDENT), StudentController.getMyCourses);
 
-router.get('/myAcademicInfo', StudentController.getMyAcademicInfos);
+router.get('/myAcademicInfo', auth(ENUM_USER_ROLE.STUDENT), StudentController.getMyAcademicInfos);
 
-router.get('/myCoursesSchedules', StudentController.getMyCourseSchedules);
+router.get(
+  '/myCoursesSchedules',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentController.getMyCourseSchedules
+);
 
 router.get('/:id', StudentController.getSingleStudent);
 
-router.patch('/:id', StudentController.updateStudent);
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.updateStudentZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  StudentController.updateStudent
+);
 
-router.delete('/:id', StudentController.deleteStudent);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  StudentController.deleteStudent
+);
 
 export const StudentRoutes = router;
