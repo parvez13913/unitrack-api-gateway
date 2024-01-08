2f62e709b27422f3179414639d61dd4eb650a180
parvez13913 <parvezz13913@gmail.com>
2024-01-08 19:07:13 +0600

    [DONE] Management Department validation

--- src/app/modules/managementDepartment/managementDepartment.route.ts
+++ src/app/modules/managementDepartment/managementDepartment.route.ts
@@ -1,16 +1,52 @@
 import express from 'express';
 import { ManagementDepartmentController } from './managementDepartment.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { ManagementDepartmentValidation } from './managementDepartment.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-router.get('/', ManagementDepartmentController.getAllDepartments);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  ManagementDepartmentController.getAllDepartments
+);
 
-router.get('/:id', ManagementDepartmentController.getSingleDepartment);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  ManagementDepartmentController.getSingleDepartment
+);
 
-router.post('/', ManagementDepartmentController.createDepartment);
+router.post(
+  '/',
+  validateRequest(ManagementDepartmentValidation.createManagementDepartmentZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  ManagementDepartmentController.createDepartment
+);
 
-router.patch('/:id', ManagementDepartmentController.updateDepartment);
+router.patch(
+  '/:id',
+  validateRequest(ManagementDepartmentValidation.updateManagementDepartmentZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  ManagementDepartmentController.updateDepartment
+);
 
-router.delete('/:id', ManagementDepartmentController.deleteDepartment);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  ManagementDepartmentController.deleteDepartment
+);
 
 export const ManagementDepartmentRoutes = router;
