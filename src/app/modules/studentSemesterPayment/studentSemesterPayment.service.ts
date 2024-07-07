76fa79a887564badd51485bd2353e1fb8440371d
parvez13913 <parvezz13913@gmail.com>
2024-07-07 14:20:26 +0600

    Code modified

--- src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
@@ -3,7 +3,7 @@ import { IGenericResponse } from '../../../interfaces/common';
 import { CoreService } from '../../../shared/axios';
 
 const getMySemesterPayments = async (req: Request): Promise<IGenericResponse> => {
-  const response: IGenericResponse = await CoreService.get('/studentSemesterPayments', {
+  const response: IGenericResponse = await CoreService.get('/studentSemesterPayments/mySemesterPayments', {
     params: req.query,
     headers: {
       Authorization: req.headers.authorization
